8ff9562de3f04ea64fe297d616a05f324d019e72
PastGhost <felipemachadocordeiro@gmail.com>
2016-06-13 19:57:44 -0300

    Update login.php

--- views/formularios/promotor/login.php
+++ views/formularios/promotor/login.php
@@ -1,7 +1,7 @@
 <?php
 	$logado = false;
 	function login($email,$password) {
-		$usuarios = json_decode(file_get_contents("users.json"));
+		$usuarios = json_decode(file_get_contents("../../../dados_json/users.json"));
 		$n = count($usuarios);
 		$i = 0;
 		while($email != $usuarios[$i]->email && $n > $i) $i++;
