ffc0e092891bdd92dabe574ca6be162436dcb809
PastGhost <felipemachadocordeiro@gmail.com>
2016-06-13 19:29:27 -0300

    Update login-usuario.php

--- login-usuario.php
+++ login-usuario.php
@@ -25,10 +25,56 @@
 	}
 ?>
 <!DOCTYPE html>
+<html>
+<head>
+	<title>Wherevent | Cadastro de Promotor</title>
+	<meta charset="UTF-8">
 
-
+	<link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
 <form method="POST" action="">
-	<input type="text" name="email">
-	<input type="text" name="password">
-	<input type="submit" >
+
+
+<header>
+	<div id="header-wrapper">
+	    <div id="header" class="container">
+	        <div id="logo">
+				<center></center><a href="http://wherevent.azurewebsites.net/"><img src="../../images/LOGO%20WHEREVENT.png"></a></center>
+	        </div>
+	    </div>
+	</div>
+
+	<ul class="nav nav-pills nav-justified thumbnail setup-panel">
+	  <li class="active">
+	    <a href="#step-1">
+	    <h4 class="list-group-item-heading">Log In</h4>
+	    <p class="list-group-item-text">Preencha seus dados</p></a>
+	  </li>
+	</ul>
+</header>
+
+<section>
+	<div class="container">
+		<div class="row">
+    		<div class="col-md-6">
+    			<div class="col-xs-12">
+				<form method="POST" action="">
+					<label>E-mail</label>
+					<input type="email" name="email" class="form-control form-group">
+
+					<label>Password</label>
+					<input type="password" name="pwd" class="form-control form-group">
+				</form>
+
+	<input type="submit" name="login" value="Login" class="btn btn-primary btn-lg">
+	</div>
+	</div></div></div>
+</section>
+
+
 </form>
+
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
